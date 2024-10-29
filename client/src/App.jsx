f15563c2efd6c684c5660116d15ec528eef30f99
tuankietbmy <tuankietbmy@gmail.com>
2024-10-29 22:00:33 +0700

    update forum

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,12 +20,15 @@ import CandidateProfile from "./pages/user-view/candidate/profile";
 import AdminUsers from "./pages/admin-view/users";
 import CandidateNews from "./pages/user-view/candidate/news";
 import RecruiterHome from "./pages/user-view/recruiter/home";
+import HRProfile from "./pages/user-view/recruiter/profile";
 import PostJob from "./pages/user-view/recruiter/postjob";
 import RecruiterLayout from "./components/user-view/recruiter/layout";
 import MyPosts from "./pages/user-view/recruiter/mypost";
 import JobDetails from "./pages/user-view/candidate/jobdetails";
 import Application from "./pages/user-view/candidate/application";
 import MyApplications from "./pages/user-view/candidate/myapplications";
+import CandidateForum from "./pages/user-view/candidate/forum";
+import HRForum from "./pages/user-view/recruiter/forum";
 function App() {
   const { user, isAuthenticated, isLoading } = useSelector((state) => state.auth);
   const dispatch = useDispatch();
@@ -87,6 +90,7 @@ function App() {
             <Route path="application/:id" element={<Application />} />
             <Route path="myapplications" element={<MyApplications />} />
             <Route path="news" element={<CandidateNews />} />
+            <Route path="forum" element={<CandidateForum />} />
           </Route>
           <Route
             path="/recruiter"
@@ -99,6 +103,8 @@ function App() {
             <Route path="home" element={<RecruiterHome />} />
             <Route path="myposts" element={<MyPosts />} />
             <Route path="postjob" element={<PostJob />} /> 
+            <Route path="profile" element={<HRProfile />} /> 
+            <Route path="forum" element={<HRForum />} /> 
           </Route>
           <Route path="/unauth-page" element={<UnauthPage />} />
           <Route path="*" element={<NotFound />} />
--- client/src/components/user-view/recruiter/header.jsx
+++ client/src/components/user-view/recruiter/header.jsx
@@ -64,6 +64,9 @@ function RecruiterHeader() {
                                 onClick={() => {
                                     if (item === 'My post') {
                                         handleNavigate('/recruiter/myposts');
+                                    }
+                                    if (item === 'Forum') {
+                                        handleNavigate('/recruiter/forum');
                                     } else {
                                         setDropdownOpen((prev) => (prev === item ? '' : item));
                                     }
@@ -72,7 +75,7 @@ function RecruiterHeader() {
                             >
                                 {item}
                             </button>
-                            {dropdownOpen === item && (
+                            {dropdownOpen === item  && item !== 'Forum' && (
                                 <ul className="absolute left-0 mt-2 w-40 bg-gray-800 rounded-lg shadow-lg z-10">
                                     {['Submenu 1', 'Submenu 2', 'Submenu 3'].map((submenu) => (
                                         <li key={submenu} className="px-4 py-2 hover:bg-gray-600 transition duration-150">{submenu}</li>
